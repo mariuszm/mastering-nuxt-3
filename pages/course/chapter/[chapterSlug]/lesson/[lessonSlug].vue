279f654a8a62d15d6b345b786819c89234603c03
Mariusz Michalski <supermariusz@gmail.com>
2024-05-15 10:44:21 +0200

    chore: usehead composable

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -36,4 +36,10 @@ const chapter = computed(() => {
 const lesson = computed(() => {
   return chapter.value.lessons.find(lesson => lesson.slug === route.params.lessonSlug)!;
 });
+
+const title = computed(() => {
+  return `${lesson.value.title} - ${course.title}`;
+});
+
+useHead({ title });
 </script>
