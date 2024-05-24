701a4e38856c366b5ff2ad99359dd89bff65d431
Mariusz Michalski <supermariusz@gmail.com>
2024-05-24 22:11:46 +0200

    chore: basic auth middleware (fake) => a login page redirect in fact

---
+++ middleware/auth.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line
+export default defineNuxtRouteMiddleware((to, from) => {
+  if (to.params.chapterSlug === '1-chapter-1') {
+    return;
+  }
+  return navigateTo('/login');
+});
