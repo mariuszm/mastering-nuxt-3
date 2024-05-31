fde315123ac8186732b043c5b38cdc162a4d5113
Mariusz Michalski <supermariusz@gmail.com>
2024-06-01 00:21:42 +0200

    chore: removed unnecessary comments (moved to github issues)

--- server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -2,18 +2,6 @@ import course from '@/server/courseData';
 import type { Course, LessonWithPath } from "@/types/course";
 
 export default defineEventHandler((event): LessonWithPath => {
-  // TODO: investigate why Hoppscotch doesn't return 405 error (response in JSON)
-  // when using POST method on .get.ts file.
-  // As a workaround, you can remove the metod from .get.ts filename and
-  // throw an error like this:
-  //   if (event.method !== 'GET') {
-  //     throw createError({
-  //       statusCode: 405,
-  //       statusMessage: 'You requested in the wrong neighborhood',
-  //     });
-  //   }
-  // More about the bug: https://github.com/nuxt/nuxt/issues/26073
-
   const { chapterSlug, lessonSlug } = event.context.params ?? {};
 
   const chapter = (course as Course).chapters.find(chapter => chapter.slug === chapterSlug);
