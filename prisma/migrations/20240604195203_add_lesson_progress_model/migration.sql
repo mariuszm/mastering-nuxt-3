67aebbffebf8b710c28c878abb2d3532bccc66ac
Mariusz Michalski <supermariusz@gmail.com>
2024-06-04 21:56:47 +0200

    fix(db): updated migrations to fix an unique relations for LessonProgress model

--- prisma/migrations/20240604195203_add_lesson_progress_model/migration.sql
+++ prisma/migrations/20240604195203_add_lesson_progress_model/migration.sql
@@ -10,5 +10,8 @@ CREATE TABLE "LessonProgress" (
     CONSTRAINT "LessonProgress_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "LessonProgress_lessonId_userEmail_key" ON "LessonProgress"("lessonId", "userEmail");
+
 -- AddForeignKey
 ALTER TABLE "LessonProgress" ADD CONSTRAINT "LessonProgress_lessonId_fkey" FOREIGN KEY ("lessonId") REFERENCES "Lesson"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
