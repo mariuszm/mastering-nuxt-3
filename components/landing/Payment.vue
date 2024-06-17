d17178c17afed3aca95bd1a75daf38df92902ad9
Mariusz Michalski <supermariusz@gmail.com>
2024-06-17 21:06:52 +0200

    chore: integrate with Stripe to handle payments

--- components/landing/Payment.vue
+++ components/landing/Payment.vue
@@ -1,7 +1,7 @@
 <template>
   <LandingModal @close="$emit('close')">
     <div class="w-full max-w-2xl p-8 bg-slate-200 rounded-xl">
-      <form>
+      <form @submit.prevent="handleSubmit">
         <h2 class="text-xl font-bold text-center">Buying {{ course.title }}</h2>
         <div class="px-8 py-6 mt-8 text-base bg-white rounded shadow-md width">
           <div class="flex items-center justify-between w-full mb-8">
@@ -14,6 +14,10 @@
               placeholder="your@email.com"
               required
             />
+          </div>
+
+          <div id="card-element">
+            <!-- Elements will create input elements here -->
           </div>
         </div>
 
@@ -29,4 +33,79 @@
 
 <script setup>
 const course = await useCourse();
+const config = useRuntimeConfig();
+const stripe = ref(null);
+const card = ref(null);
+const email = ref('');
+const processingPayment = ref(false);
+const success = ref(false);
+
+const formStyle = {
+  base: {
+    fontSize: '16px',
+    color: '#3d4852',
+    '::placeholder': {
+      color: '#8795a1',
+    },
+  },
+};
+
+const elements = computed(() => stripe.value.elements());
+
+const setupStripe = () => {
+  stripe.value = Stripe?.(config.public.stripeKey);
+
+  if (!card.value && elements.value) {
+    card.value = elements.value.create('card', {
+      style: formStyle,
+    });
+    card.value.mount('#card-element');
+  }
+};
+
+const handleSubmit = async () => {
+  if (email.value === '') return;
+
+  processingPayment.value = true;
+  let secret;
+
+  try {
+    // create a PaymentIntent with the order amount and currency
+    const response = await $fetch('/api/stripe/paymentIntent', {
+      method: 'POST',
+      body: {
+        email: email.value,
+      },
+    });
+    secret = response;
+  } catch (error) {
+    console.error(error);
+  }
+
+  try {
+    const response = await stripe.value.confirmCardPayment(secret, {
+      payment_method: {
+        card: card.value,
+      },
+      receipt_email: email.value,
+    });
+
+    if (response.paymentIntent.status === 'succeeded') {
+      success.value = true;
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    processingPayment.value = false;
+  }
+};
+
+useHead({
+  script: [
+    {
+      src: 'https://js.stripe.com/v3',
+      onload: setupStripe,
+    },
+  ],
+});
 </script>
