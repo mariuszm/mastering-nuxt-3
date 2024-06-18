79364fb97ab24e081a878ab35ee44f1b5c5215f8
Mariusz Michalski <supermariusz@gmail.com>
2024-06-18 23:51:08 +0200

    chore: create CoursePurchase object in the paymentIntent endpoint

--- server/api/stripe/paymentIntent.post.ts
+++ server/api/stripe/paymentIntent.post.ts
@@ -1,14 +1,41 @@
+import { PrismaClient } from '@prisma/client';
 import Stripe from './stripe';
+
+const prisma = new PrismaClient();
 
 export default defineEventHandler(async event => {
   const { email } = await readBody(event);
 
   // we only have one course for now, so we have the price hard-coded
-  const paymentIntent = await Stripe.paymentIntents.create({
-    amount: 97 * 100,
-    currency: 'usd',
-    metadata: { email },
-  });
+  let paymentIntent;
+  try {
+    paymentIntent = await Stripe.paymentIntents.create({
+      amount: 97 * 100,
+      currency: 'usd',
+      metadata: { email },
+    });
+  } catch (error) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Error creating payment intent',
+    });
+  }
+
+  // create a course purchase record
+  try {
+    await prisma.coursePurchase.create({
+      data: {
+        userEmail: email,
+        courseId: 1, // hard code this value for now
+        paymentId: paymentIntent.id,
+      },
+    });
+  } catch (error) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Error creating course purchase',
+    });
+  }
 
   // only return the secret
   return paymentIntent.client_secret;
